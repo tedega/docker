4223b97e2e05b78107bb0d6c22abd4af9288a6ab
Torsten Irländer <torsten@irlaender.de>
2020-05-15 09:23:27 +0200

    Test fluentd logging in local docker container.

--- fluentd/test.sh
+++ fluentd/test.sh
@@ -1,6 +1,7 @@
+set -x
 IP=`docker inspect -f '{{.NetworkSettings.IPAddress}}' tedega-fluent-logger`
 docker run \
   --log-driver=fluentd \
-  --log-opt tag="docker.{{.ID}}" \
-  --log-opt fluentd-address=${IP}:24224 \
+  --log-opt tag="docker.test" \
+  --log-opt fluentd-address=localhost:24224 \
   python:alpine echo Hello
